ed4de2c89d599a56554684413683cbea36e85c25
ghofrane137 <tzrghofrane@gmail.com>
2024-01-02 17:35:18 +0100

    Create drawButton.c

---
+++ try/drawButton.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+
+const int SCREEN_WIDTH = 1000;
+const int SCREEN_HEIGHT = 700;
+int MOVE_SPEED = 2;
+
+void drawButton(SDL_Renderer *Renderer, buttons button, TTF_Font *font, const char *buttonText, SDL_Color textColor) {
+    SDL_SetRenderDrawColor(Renderer, 0, 255, 0, 255);
+    SDL_RenderFillRect(Renderer, &button.button);
+
+    SDL_Surface *buttonSurface = TTF_RenderText_Solid(font, buttonText, textColor);
+    SDL_Texture *buttonTexture = SDL_CreateTextureFromSurface(Renderer, buttonSurface);
+
+    SDL_Rect buttonTextRect = {
+        button.button.x + button.button.w / 4,
+        button.button.y + button.button.h / 4,
+        buttonSurface->w,
+        buttonSurface->h};
+
+    SDL_RenderCopy(Renderer, buttonTexture, NULL, &buttonTextRect);
+
+    SDL_FreeSurface(buttonSurface);
+    SDL_DestroyTexture(buttonTexture);
+}
